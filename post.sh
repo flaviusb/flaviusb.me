6fed996ddf34cc4ede3ef0d41d39dc73f5b50f56
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2011-01-12 17:16:49 +1300

    Curl to correct path to force regeneration of routes.

--- post.sh
+++ post.sh
@@ -4,4 +4,4 @@ coffee -c post.coffee
 node post.js "$1"
 git commit flaviusb.json -m "Automated commit of generated tweet from flaviusb.me."
 coffee gentweets.coffee
-curl http://flaviusb.me/commands/regenroutes
+curl http://flaviusb.me/command/regenroutes
